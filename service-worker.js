fd6a5f272d77b772ddd2ca909bf1cbdbed0e2458
kishigami taisa <jazy5kbji@icloud.com>
2024-11-16 17:32:21 +0900

    Fixed manifest.json and updated icons

--- service-worker.js
+++ service-worker.js
@@ -28,20 +28,25 @@ self.addEventListener('install', event => {
 
 // フェッチイベント
 self.addEventListener('fetch', event => {
+  if (event.request.method === 'POST') {
+      // POSTリクエストはキャッシュせず直接ネットワークへ
+      return;
+  }
   event.respondWith(
-      fetch(event.request)
-          .then(networkResponse => {
-              return caches.open(CACHE_NAME).then(cache => {
-                  cache.put(event.request, networkResponse.clone());
-                  return networkResponse;
+      caches.match(event.request)
+          .then(cachedResponse => {
+              if (cachedResponse) {
+                  return cachedResponse;
+              }
+              return fetch(event.request).then(networkResponse => {
+                  return caches.open(CACHE_NAME).then(cache => {
+                      cache.put(event.request, networkResponse.clone());
+                      return networkResponse;
+                  });
               });
-          })
-          .catch(() => {
-              return caches.match(event.request);
           })
   );
 });
-
 
 // 古いキャッシュを削除するためのアクティベートイベント
 self.addEventListener('activate', event => {
